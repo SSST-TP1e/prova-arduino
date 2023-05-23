a5c5e4044e1f32c9608a5ba43dd607e6d06a2640
Ambrosini Reto <128051061+reto-ssst@users.noreply.github.com>
2023-05-23 15:45:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Arduino Mega2560 
+
+Questa repository contiene i file necessari per iniziare a programmare un Arduino Mega2560 munito di scheda SAM.
+
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+#define VCC 4.972	// tensione di alimentazione
+
+// potenziometri
+#define POT_1 A6
+#define POT_2 A7
+
+// tasti
+#define S_1 62
+#define S_2 63
+
+//LED
+#define LED_PWM 12
+
+void setup() 
+{
+  // put your setup code here, to run once:
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
